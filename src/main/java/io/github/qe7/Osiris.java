0f44f907170070a13709d5e62d091b594ff506bc
shae <89613516+qe7@users.noreply.github.com>
2024-08-21 15:41:31 +0100

    1.0.4
    Hotfix for scaffold crash

--- src/main/java/io/github/qe7/Osiris.java
+++ src/main/java/io/github/qe7/Osiris.java
@@ -26,7 +26,7 @@ public final class Osiris {
         System.out.println("Osiris instance created!");
 
         this.name = "Osiris";
-        this.version = "1.0.3";
+        this.version = "1.0.4";
 
         this.eventBus = new EventBus();
 
