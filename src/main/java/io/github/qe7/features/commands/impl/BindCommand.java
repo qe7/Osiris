33a2d25b3bd3e273c000f77848754f9b525bd372
shae <89613516+qe7@users.noreply.github.com>
2024-08-23 16:14:07 +0100

    Bind command
    [#] Bind command - Recoded

--- src/main/java/io/github/qe7/features/commands/impl/BindCommand.java
+++ src/main/java/io/github/qe7/features/commands/impl/BindCommand.java
@@ -26,29 +26,35 @@ public class BindCommand extends Command {
         final String moduleName = args[1];
         final String key = args[2];
 
-        for (Module module : Osiris.getInstance().getModuleManager().getMap().values()) {
-            if (module.getName().replace(" ", "").equalsIgnoreCase(moduleName)) {
-                if (key.equalsIgnoreCase("none")) {
-                    module.setKeyCode(-1);
-                    ChatUtil.addPrefixedMessage("Bind", "Unbound " + module.getName());
-                    return;
-                }
-
-                int keyCode;
-
-                try {
-                    keyCode = Keyboard.getKeyIndex(key.toUpperCase());
-                } catch (NumberFormatException e) {
-                    ChatUtil.addPrefixedMessage("Bind", "Invalid key");
-                    return;
-                }
-
-                module.setKeyCode(keyCode);
-                ChatUtil.addPrefixedMessage("Bind", "Bound " + module.getName() + " to " + key);
-                return;
+        Module module = null;
+        
+        for (Module m : Osiris.getInstance().getModuleManager().getMap().values()) {
+            if (m.getName().replace(" ", "").equalsIgnoreCase(moduleName)) {
+                module = m;
+                break;
             }
         }
+        
+        if (module == null) {
+            ChatUtil.addPrefixedMessage("Bind", "Module not found");
+            return;
+        }
 
-        ChatUtil.addPrefixedMessage("Bind", "Module not found");
+        if (key.equalsIgnoreCase("none")) {
+            module.setKeyCode(-1);
+            ChatUtil.addPrefixedMessage("Bind", "Unbound " + module.getName());
+            return;
+        }
+
+        int keyCode;
+
+        try {
+            keyCode = Keyboard.getKeyIndex(key.toUpperCase());
+        } catch (NumberFormatException e) {
+            ChatUtil.addPrefixedMessage("Bind", "Invalid key");
+            return;
+        }
+
+        module.setKeyCode(keyCode);
     }
 }
